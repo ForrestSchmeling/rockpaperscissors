d771498f5fadf312e302cb760369702f3d6ffbd3
Forrest Schmeling <Forrestschmeling@icloud.com>
2022-05-23 19:52:05 -0700

    added the shell of the game with possible pictures later

---
+++ game.js
@@ -0,0 +1,11 @@
+function rock(){
+    alert("You have chosen Rock!");
+}
+function paper(){
+    alert("You have chosen Paper!");
+}
+
+function scissors(){
+    alert("You have chosen Scissors!");
+}
+
